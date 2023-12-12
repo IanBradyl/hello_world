d4003331bd6037142ad8c2cad8a34faea8dba423
sunless13 <sunlesskelv@gmail.com>
2023-12-12 08:45:30 +0300

    added java hello world

---
+++ hello_world.java
@@ -0,0 +1,10 @@
+// declaring class first? ... java is weird
+class helloWorld {
+
+	// one day i'll know what all this means
+	public static void main (String[] args)
+	{
+		// classic first program
+		System.out.println("Hello World!");
+	}
+}
